f075a47af229dcdf0c8fc11fad35630943029510
Ali Haider <125692810+officialalihaider@users.noreply.github.com>
2023-10-08 12:27:38 +0000

    datatype

--- 01_basics/01_datatypes.js
+++ 01_basics/01_datatypes.js
@@ -2,3 +2,17 @@
 // alert ( 3 + 3 ) // we are using nodeJs, not browser
 console.log(3 + 3);
 console.log("Ali Haider");
+
+
+// number => 2 to power 53
+// bigint
+// string => ""
+// null => standalone value
+// undefine => 
+// symbol => unique
+
+
+//object 
+
+console.log(typeof undefined); //undefined
+console.log(typeof null); //object
