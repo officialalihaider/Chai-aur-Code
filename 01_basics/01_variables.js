6605780af1734cb48919988936fb0b5d911bc0e4
Ali Haider <125692810+officialalihaider@users.noreply.github.com>
2023-10-08 12:23:46 +0000

    datatypes

---
+++ 01_basics/01_variables.js
@@ -0,0 +1,6 @@
+const accountId = 25474327
+let accountEmail = "Aliseeyam@gmailcom"
+var accountPassword = "26454"
+let accountState;
+
+console.table([accountEmail , accountId , accountPassword, accountState]);
