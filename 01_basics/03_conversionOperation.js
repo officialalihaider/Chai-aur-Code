6f5f881ea9f83ed00281eddae1e8971888e6068c
Ali Haider <125692810+officialalihaider@users.noreply.github.com>
2023-10-08 13:01:49 +0000

    conversion of datatype

---
+++ 01_basics/03_conversionOperation.js
@@ -0,0 +1,29 @@
+ let score = "44"
+
+//  console.log(typeof score);
+//  console.log(typeof(score));
+
+ let valueNumber = Number(score)
+//  console.log(typeof valueNumber);
+
+//  console.log(valueNumber);
+
+ //  "33" => 33
+ // "33abc" => NaN
+ // true => 1; fasle => 0
+
+ let isLoggedIn =  0
+ let booleanIsLoggedIn = Boolean(isLoggedIn)
+//  console.log(booleanIsLoggedIn);
+
+
+
+ // 1 => true; 0 => false
+ // "" => false
+ // "Ali Haider" => true
+
+
+ let someNumber = 33
+
+ let stringNumber = String(someNumber)
+ console.log(stringNumber) 
