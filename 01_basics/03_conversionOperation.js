74a297234aa7a0b34db8dc4bd2b0714af77bc2d5
Ali Haider <125692810+officialalihaider@users.noreply.github.com>
2023-10-08 13:28:40 +0000

    comaprision done

--- 01_basics/03_conversionOperation.js
+++ 01_basics/03_conversionOperation.js
@@ -26,4 +26,29 @@
  let someNumber = 33
 
  let stringNumber = String(someNumber)
- console.log(stringNumber) 
+//  console.log(stringNumber)  
+
+ // *************** Operations ******************
+
+ let value = 3
+ let negValue = -value
+//  console.log(negValue);
+
+// console.log(2 + 3);
+
+let str1 = "hello"
+let str2 = " Ali Haider"
+let str3 = str1 + str2
+console.log(str3)
+
+// console.log("1" + 2);
+// console.log(1 + "2");
+// console.log("1" + 2 + 2);
+// console.log(1 + 2 + "2");
+
+
+// console.log(+true);
+// console.log(true+);
+// console.log(+"");
+
+
